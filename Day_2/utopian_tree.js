56ac3adf4a974ac9d30b09773e22a949ad7aaeda
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-26 23:35:34 +0530

    Added Javascript Source File named utopian_tree.js for HackerRank Challenge named Utopian Tree

---
+++ Day_2/utopian_tree.js
@@ -0,0 +1,11 @@
+function utopianTree(n) {
+    var initialHeight = 1;
+    if (n == 0) return 1;
+
+    for (let i = 1; i <= n; i++){
+        if (i % 2 == 0) initialHeight += 1;
+        else initialHeight *= 2;
+    }
+    return initialHeight;
+
+}
