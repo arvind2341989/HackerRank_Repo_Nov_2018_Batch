0f854aff75df6e14e8e2bdfd502e92fb65db8568
nithilareddy <Nithilareddy.g@tadigital.com>
2018-12-26 18:49:09 +0530

    Added JavaScript Source File named intro_tutorial.js for HackerRank Challenge named Intro Tutorial

---
+++ intro_tutorial.js
@@ -0,0 +1,12 @@
+function introTutorial(V, arr) {
+    var a = 0
+    for (var i in arr) {
+        if (arr[i] == V)
+            return a;
+
+        else {
+            a += 1
+        }
+    }
+
+}
