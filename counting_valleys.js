4daf0b80ce3c2ba73b5c0d783c650d6e51100c76
Pavan Kumar Reddy Chinta <c.pavankumarreddy@yahoo.com>
2018-12-26 18:27:21 +0530

    Added JavaScript Source File named counting_valleys.js for HackerRank Challenge named Counting Valleys

---
+++ counting_valleys.js
@@ -0,0 +1,15 @@
+function countingValleys(n, s) {
+    var i = 0, count = 0, valley = 0;
+    for (i = 0; i < n; i++) {
+        if (s[i]=='D') {
+            count--;
+        }
+        else if(s[i]=='U') {
+            count++;
+            if (count == 0) {
+                valley++;
+            }
+        }
+    }
+    return valley;
+}
