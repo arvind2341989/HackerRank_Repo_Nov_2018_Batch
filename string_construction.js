adf83a199f11f741ba50941c125c899fc9a17e86
Krishna Vaishnavi Makam <navee.vaish@gmail.com>
2018-12-26 20:15:28 +0530

    Added JavaScript Source File named string_construction.js for HackerRank Challenge named String Construction.

---
+++ string_construction.js
@@ -0,0 +1,15 @@
+
+// Complete the stringConstruction function below.
+function stringConstruction(s) {
+    var mySet = new Set();
+    var cost = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (!mySet.has(s.charAt(i))) {
+            mySet.add(s.charAt(i));
+            cost++;
+        }
+    }
+    return cost;
+
+
+}
