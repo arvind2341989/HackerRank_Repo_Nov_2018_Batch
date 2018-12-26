21dc926f318d663267e9117b273a162500324254
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-26 22:56:30 +0530

    Added Javascript Source File named angry_professor.js for HackerRank Challenge named Angry Professor

---
+++ Day_2/angry_professor.js
@@ -0,0 +1,10 @@
+// Complete the angryProfessor function below.
+function angryProfessor(k, a) {
+    var count = 0;
+    for (let i = 0; i < a.length; i++){
+        if (a[i] <= 0) count++;
+    }
+    if (count >= k) return "NO";
+    else return "YES";
+
+}
