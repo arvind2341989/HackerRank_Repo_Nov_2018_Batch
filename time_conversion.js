990734c3c3faaa67e927fedcdf8ebee3db40832a
Charan Teja Madabathula <mcharan1996@gmail.com>
2018-12-26 18:41:36 +0530

    Added Javascript Source File named time_conversion.js for HackerRank Challenge named Time Conversion

---
+++ time_conversion.js
@@ -0,0 +1,24 @@
+function timeConversion(s) {
+    /*
+     * Write your code here.
+     */
+    var time = s.split(':');
+    var am_pm = time[2].substring(2,4);
+    if (am_pm == "PM") {
+        if (time[0] == '12') {
+            time[0] = '12';
+        }
+        else {
+            time[0] = parseInt(time[0]) + 12;
+            console.log(time[0]);
+        }
+    }
+    else if (am_pm == 'AM') {
+        if (time[0] == '12') {
+            time[0] = '00';
+        }
+    }
+    var a = time[0] + ':' + time[1] + ':' + time[2].substring(0,2);
+    return a;
+
+}
