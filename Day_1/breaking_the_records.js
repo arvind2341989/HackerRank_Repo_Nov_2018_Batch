4b360666c1f26198facb4bd43f2447fde5395fd5
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-26 22:41:58 +0530

    Added Javascript Source File named breaking_the_records.js for HackerRank Challenge named Breaking The Records

---
+++ Day_1/breaking_the_records.js
@@ -0,0 +1,19 @@
+function breakingRecords(scores) {
+    var countHigh = 0;
+    var countLow = 0;
+    var max = scores[0];
+    var min = scores[0];
+    for (let i = 0; i < scores.length; i++){
+        if (scores[i] > max) {
+            max = scores[i];
+            countHigh++;
+        }
+        else if (scores[i] < min) {
+            min = scores[i];
+            countLow++;
+        }
+        else continue;
+    }
+    return [countHigh,countLow];
+
+}
