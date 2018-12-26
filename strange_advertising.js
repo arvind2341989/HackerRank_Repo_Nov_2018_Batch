04f3fa199a67ee49d1532783fb6b325458a9d56a
Krishna Vaishnavi Makam <navee.vaish@gmail.com>
2018-12-26 20:14:21 +0530

    Added JavaScript Source File named strange_advertising.js for HackerRank Challenge named Strange Advertising.

---
+++ strange_advertising.js
@@ -0,0 +1,13 @@
+function viralAdvertising(n) {
+    var totalLiked = 0;
+    var people = 5;
+    for (let i = 0; i < n; i++) {
+        const dayLiked = Math.floor(people / 2);
+        const received = dayLiked * 3;
+        totalLiked += dayLiked;
+        people = received;
+    }
+    return totalLiked;
+
+
+}
