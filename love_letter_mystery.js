1d92f8d3751b5c56ae1441f63897883202d4b75e
Kowshik Reddy Beeravolu <kowshikreddy137@gmail.com>
2018-12-26 18:52:29 +0530

    Added JavaScript Source File named love_letter_mystery.js for HackerRank Challange named Love Letter Mistery

---
+++ love_letter_mystery.js
@@ -0,0 +1,9 @@
+// Complete the theLoveLetterMystery function below.
+function theLoveLetterMystery(s) {
+var half = Math.floor(s.length / 2);
+   var diff = 0
+   for (let i = 0; i < half; i++) {
+        diff += Math.abs(s.charCodeAt(i) - s.charCodeAt(s.length - i - 1))
+    }
+    return diff;
+}
