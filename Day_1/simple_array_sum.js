635a7206a84f7670add83749d70a513d761d0fbd
Aaditya.r <rajaadirash9631@gmail.com>
2018-12-27 12:15:56 +0530

    Added Javascript Source File named simple_array_sum.js for HackerRank Challenge named Simple Array Sum

---
+++ Day_1/simple_array_sum.js
@@ -0,0 +1,11 @@
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    var i;
+    var sum = 0;
+    for (i = 0; i < ar.length; i++){
+        sum = sum + ar[i];
+    }
+    return sum;
+}
