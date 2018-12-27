9c87a5848bdd9dcaeff7a27c30897865f33acd34
Ravali.Talluri <talluriravali@gmail.com>
2018-12-27 11:44:33 +0530

    Added JavaScript Source File named day_2\cat_and_mouse.js  for HackerRank Challenge named Cats and a Mouse.

---
+++ day_2/cat_and_mouse.js
@@ -0,0 +1,9 @@
+ if (Math.abs(x - z) < Math.abs(y - z)) {
+        return "Cat A";
+    }
+    else if (Math.abs(x - z) > Math.abs(y - z)) {
+        return "Cat B";
+    }
+    else {
+        return "Mouse C";
+    }
