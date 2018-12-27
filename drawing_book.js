5d27dff92f1da365b520d66181600bc6898a10dc
Krishna Vaishnavi Makam <navee.vaish@gmail.com>
2018-12-27 10:54:11 +0530

    delete all files

--- drawing_book.js
+++
@@ -1,8 +0,0 @@
-function pageCount(n, p) {
-    /*
-     * Write your code here.
-     */
-    var leftStart = Math.floor(p / 2),
-        rightStart = Math.floor(n / 2) - leftStart;
-    return leftStart > rightStart ? rightStart : leftStart;
-}
--- strange_counter.js
+++
@@ -1,9 +0,0 @@
-// Complete the strangeCounter function below.
-function strangeCounter(t) {
-    var rem = 4;
-    while (rem <= t) {
-        rem = rem * 2 + 2;
-    }
-    return (rem - t);
-
-}
