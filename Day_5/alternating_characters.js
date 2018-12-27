2a2df7d9d8049bb3360e8ddec67e2cf95c9d79dd
Pavan Kumar Reddy Chinta <c.pavankumarreddy@yahoo.com>
2018-12-27 12:42:42 +0530

    Added JavaScript Source File named alternating_characters.js for HackerRank Challenge named Alternating Characters.

---
+++ Day_5/alternating_characters.js
@@ -0,0 +1,11 @@
+function alternatingCharacters(s) {
+    var count = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        if (s.charAt(i) == s.charAt(i + 1))
+            count++;
+    }
+
+    return count;
+
+}
