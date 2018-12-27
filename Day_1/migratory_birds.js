508c5af2cd7765f66b505d1f838fa60ead8729f3
Krishna Vaishnavi Makam <navee.vaish@gmail.com>
2018-12-27 11:52:46 +0530

    Added JavaScript Source File named migratory_birds.js for HackerRank Challenge named Migratory Birds.

---
+++ Day_1/migratory_birds.js
@@ -0,0 +1,16 @@
+function migratoryBirds(arr) {
+    var i = 0, ar = [0, 0, 0, 0, 0, 0], max = 0;
+    for (i = 0; i < arr.length; i++) {
+        ar[arr[i]]++;
+    }
+    for (i = 1; i < 6; i++) {
+        if (ar[i] >= max) {
+            max = ar[i];
+        }
+    }
+    for (i = 1; i < 6; i++) {
+        if (ar[i] == max) {
+            return i;
+        }
+    }
+}
