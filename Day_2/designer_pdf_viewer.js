e7e09f9db80929845c7f94ba6f4af5c12c8cff93
Avneesh Kohli <avneeshkohli3@gmail.com>
2018-12-27 10:56:12 +0530

    Added Javascript Source File named designer_pdf_viewer.js for HackerRank Challenge named Designer PDF Viewer

---
+++ Day_2/designer_pdf_viewer.js
@@ -0,0 +1,8 @@
+function designerPdfViewer(h, word) {
+    var max = 0;
+    for (let i = 0; i < h.length; i++) {
+        if ((h[word.charCodeAt(i) - 97]) > max)
+            max = h[word.charCodeAt(i) - 97];
+    }
+    return max * word.length;
+}
