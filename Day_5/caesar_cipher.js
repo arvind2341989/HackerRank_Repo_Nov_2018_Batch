5cf398aafb809938fc21984476da3a1b37f7265d
MithraTalluri <tlns.mithra@gmail.com>
2018-12-27 16:09:37 +0530

    Added JavaScript Source File named caesar_cipher.js for HackerRank Challenge named Caesar Cipher.

---
+++ Day_5/caesar_cipher.js
@@ -0,0 +1,21 @@
+function caesarCipher(s, k) {
+    var str = "";
+    for (var i = 0; i < s.length; i++) {
+        //for lowercase
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            var c = ((s.charCodeAt(i) + k - 97) % 26) + 97;
+            var char = String.fromCharCode(c);
+            str = str + char;
+        }
+         //for uppercase
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            var c = ((s.charCodeAt(i) + k - 65) % 26) + 65;
+            var char = String.fromCharCode(c);
+            str = str + char;
+        }
+        else {
+            str = str + s[i];
+        }
+    }
+    return str;
+}
