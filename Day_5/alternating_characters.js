f320d7cfb7878da614276066c371e7897cccc97a
Charan Teja Madabathula <mcharan1996@gmail.com>
2018-12-27 14:00:33 +0530

    Added JavaScript Source File named alternating_characters.js for HackerRank Challenge named Alternating Characters.

---
+++ Day_5/alternating_characters.js
@@ -0,0 +1,9 @@
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    var count = 0;
+    for (var i = 0; i < s.length; i++) {
+        if (s.charAt(i) == s.charAt(i + 1))
+            count++;
+    }
+    return count;
+}
