fb83fa4f034d99af3c0a3b8edad1227d2bc8d9ab
Manisha Annamalaisamy <manisha665@gmail.com>
2018-12-27 12:08:52 +0530

    Added JavaScript Source File named Day_5\sherlock_and_anagrams.js  for HackerRank Challenge named Sherlock and Anagrams.

---
+++ Day_5/sherlock_and_anagrams.js
@@ -0,0 +1,22 @@
+function sherlockAndAnagrams(s) {
+    let count = 0;
+    var m = {};
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i + 1; j <= s.length; j++) {
+            var str = s.substring(i, j).split('').sort().join('');
+            if (str in m) {
+                m[str].push(i)
+            } else {
+                m[str] = [i]
+            }
+        }
+    }
+
+    for (let i in m) {
+        let item = m[i].length;
+        if (item > 1) {
+            count += fact(item) / (fact(2) * fact(item - 2));
+        }
+    }
+    return count;
+}
