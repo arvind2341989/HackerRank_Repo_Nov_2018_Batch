63831df5595b693914954812ccaba7694adf96e9
MithraTalluri <tlns.mithra@gmail.com>
2018-12-27 12:45:38 +0530

    Added JavaScript Source File named simple_array_sum.js for HackerRank Challenge named Simple Array Sum.

---
+++ Day_1/simple_array_sum.js
@@ -0,0 +1,10 @@
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    var total = 0;
+    for (var i = 0; i < ar.length; i++) {
+        total += ar[i];
+    }
+    return total;
+}
