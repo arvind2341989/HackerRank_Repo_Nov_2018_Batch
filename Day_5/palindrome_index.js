d93a8813ff3792f6dc4d14c41de83d5d4612a098
MithraTalluri <tlns.mithra@gmail.com>
2018-12-27 16:05:26 +0530

    Added JavaScript Source File named palindrome_index.js for HackerRank Challenge named Palindrome Index.

---
+++ Day_5/palindrome_index.js
@@ -0,0 +1,16 @@
+function palindromeIndex(s) {
+    for (var i = 0, j = s.length - 1; i < j; i++ , j--)
+    if (s.charAt(i) != s.charAt(j))
+        if (isPalindrome(s, i))
+            return i;
+        else if (isPalindrome(s, j))
+            return j;
+    return -1;
+}
+function isPalindrome(s, index){
+    for (var i = index + 1, j = s.length - 1 - index; i < j; i++ , j--) {
+        if (s.charAt(i) != s.charAt(j))
+            return false;
+    }
+    return true;
+}
