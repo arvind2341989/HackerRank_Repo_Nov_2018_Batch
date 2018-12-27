cafffbeb620cf8b3cb6e59c21abf5a891e32dd69
Avneesh Kohli <avneeshkohli3@gmail.com>
2018-12-27 10:32:39 +0530

    Added Javascript Source File named simple_array_sum.js for HackerRank Challenge named Simple Array Sum

---
+++ Day_1/simple_array_sum.js
@@ -0,0 +1,11 @@
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    var i, sum = 0;
+
+    for (i = 0; i < ar.length; i++) {
+        sum += ar[i];
+    }
+    return sum;
+}
