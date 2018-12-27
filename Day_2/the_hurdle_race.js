6fb4d8448857e3373f3912f605a490cdaff82fd6
Avneesh Kohli <avneeshkohli3@gmail.com>
2018-12-27 11:18:17 +0530

    Added Javascript Source File named the_hurdle_race.js for HackerRank Challenge named The Hurdle Race

---
+++ Day_2/the_hurdle_race.js
@@ -0,0 +1,5 @@
+function hurdleRace(k, height) {
+    height.sort(function (a, b) { return a - b; });
+    if (k > height[height.length - 1]) return 0;
+    else return (height[height.length - 1] - k);
+}
