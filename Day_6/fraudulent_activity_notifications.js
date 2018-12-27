0ceba6edda9d129147c53fa96d43977e2a2cee57
Charan Teja Madabathula <mcharan1996@gmail.com>
2018-12-27 14:25:09 +0530

    Added JavaScript Source File named fraudulent_activity_notifications.js for HackerRank Challenge named Fraudulent Activity Notifications.

---
+++ Day_6/fraudulent_activity_notifications.js
@@ -0,0 +1,81 @@
+function findMedian(arr, d) {
+
+    if (d % 2 == 1) {
+
+        const medIdx = Math.floor(d / 2);
+
+        let start = -1;
+
+        let curIdx = 0;
+
+        while (curIdx <= medIdx) {
+
+            start += 1;
+
+            curIdx += arr[start];
+
+
+
+        }
+
+        return start;
+
+    } else {
+
+        const medIdx2 = Math.floor(d / 2);
+
+        const medIdx1 = Math.floor(d / 2) - 1;
+
+        let start = -1;
+
+        let curIdx = 0;
+
+        while (curIdx <= medIdx1) {
+
+            start += 1;
+
+            curIdx += arr[start];
+
+        }
+
+        const index1 = start;
+
+        while (curIdx <= medIdx2) {
+
+            start += 1;
+
+            curIdx += arr[start];
+
+        }
+
+        const index2 = start;
+
+        return (index1 + index2) / 2
+
+    }
+
+}
+
+
+// Complete the activityNotifications function below.
+function activityNotifications(expenditure, d) {
+    var n = expenditure.length;
+    var    notifications = 0;
+    if (d >= n) {
+        return 0;
+    }
+    var memory = new Array(201);
+    memory.fill(0);
+    for (var i = 0; i < d; i++) {
+        memory[expenditure[i]] += 1;
+    }
+    for (var i = d; i < expenditure.length; i++) {
+        const median = findMedian(memory, d);
+        if (expenditure[i] >= 2 * median) {
+            notifications += 1;
+        }
+        memory[expenditure[i - d]] -= 1;
+        memory[expenditure[i]] += 1;
+    }
+    return notifications;   
+}
