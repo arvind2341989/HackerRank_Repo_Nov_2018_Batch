1f7e2814c0722590426f467f4403f94c478d8fd4
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-27 11:21:49 +0530

    Added Javascript Source File named library_fine.js for HackerRank Challenge named Library Fine

---
+++ Day_3/library_fine.js
@@ -0,0 +1,14 @@
+// Complete the libraryFine function below.
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    var fine = 0;
+    if (y1 > y2) {
+        fine = 10000;
+    }
+    else if (y1 == y2) {
+        if (m1 > m2) fine = 500 * (m1 - m2);
+        else if (m1 == m2) {
+            if (d1 > d2) fine = 15 * (d1 - d2);
+        }
+    }
+    return fine;
+}
