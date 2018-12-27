3f9f60a8a6733a1fc2564f9d48eb4dd5eb5755fc
MithraTalluri <tlns.mithra@gmail.com>
2018-12-27 15:28:13 +0530

    Added JavaScript Source File named flatland_space_stations.js for HackerRank Challenge named Flatland Space Stations.

---
+++ Day_4/flatland_space_stations.js
@@ -0,0 +1,15 @@
+function flatlandSpaceStations(n, c) {
+    var diff;
+    var dist = [];
+    for (var i = 0; i < n; i++) {
+        var min = Number.MAX_VALUE;
+        for (var j = 0; j < c.length; j++) {
+            diff = Math.abs(i - c[j]);
+            if (min > diff) {
+                min = diff;
+            }
+        }
+        dist.push(min);
+    }
+    return Math.max.apply(0, dist);
+}
