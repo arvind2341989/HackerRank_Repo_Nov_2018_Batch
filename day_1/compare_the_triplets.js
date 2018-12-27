c9d19a88995162facd8774bb23365f33f6c37754
Ravali.Talluri <talluriravali@gmail.com>
2018-12-27 11:20:25 +0530

    Added JavaScript Source File named day_1/compare_the_triplets.js for HackerRank Challenge named Compare the Triplets.

---
+++ day_1/compare_the_triplets.js
@@ -0,0 +1,15 @@
+function compareTriplets(a, b) {
+   
+        var alice = 0, bob = 0;
+
+        for (var i = 0; i < 3; i++) {
+            if (a[i] < b[i]) {
+                bob++;
+            }
+            else if (a[i] > b[i]) {
+                alice++;
+            }
+        }
+        var result = [alice, bob];
+        return result;
+    }
