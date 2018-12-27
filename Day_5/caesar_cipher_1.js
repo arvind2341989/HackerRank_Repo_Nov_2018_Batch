579d9b13d7ede6202dcbf12c5c6bf8333eca2cad
Krishna Vaishnavi Makam <navee.vaish@gmail.com>
2018-12-27 12:32:00 +0530

    Added JavaScript Source File named Day_5\caesar_cipher_1.js  for HackerRank Challenge named Caesar Cipher.

---
+++ Day_5/caesar_cipher_1.js
@@ -0,0 +1,23 @@
+function caesarCipher(s, k) {
+    var str = "";
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            var c = ((s.charCodeAt(i) + k - 97) % 26) + 97;
+            var char = String.fromCharCode(c);
+            str = str + char;
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            var c = ((s.charCodeAt(i) + k - 65) % 26) + 65;
+            var char = String.fromCharCode(c);
+
+            str = str + char;
+
+        }
+        else {
+
+            str = str + s[i];
+        }
+    }
+    return str;
+
+}
