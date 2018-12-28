dc104d5cea48ec1f90bce30caf020d99801e0a1f
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-28 15:58:33 +0530

    Added Javascript Source File named counting_sort_1.js for HackerRank Challenge named Counting Sort 1

---
+++ Day_6/counting_sort_1.js
@@ -0,0 +1,6 @@
+// Complete the countingSort function below.
+function countingSort(arr) {
+    let counts = Array(100).fill(0);
+    arr.forEach(item => { ++counts[item]; });
+    return counts;
+}
