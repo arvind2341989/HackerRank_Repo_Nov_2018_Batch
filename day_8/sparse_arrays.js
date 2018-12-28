940609ea542ded98e12d28f8f397731a796ee167
Ravali.Talluri <talluriravali@gmail.com>
2018-12-28 09:53:39 +0530

    Added JavaScript Source File named day_8\sparse_arrays.js  for HackerRank Challenge named Sparse Arrays.

---
+++ day_8/sparse_arrays.js
@@ -0,0 +1,13 @@
+// Complete the matchingStrings function below.
+function matchingStrings(strings, queries) {
+    var result = new Array(queries.length).fill(0);
+    for (var v = 0; v < queries.length; v++) {
+        for (var z = 0; z < strings.length; z++) {
+            if (queries[v] == strings[z]) {
+                result[v]++;
+            }
+        }
+    }
+    return result;
+
+}
