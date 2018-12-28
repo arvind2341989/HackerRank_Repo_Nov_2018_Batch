47084f4a7c199ae4035f2a9998bac19332a19a86
Avneesh Kohli <avneeshkohli3@gmail.com>
2018-12-28 16:33:28 +0530

    Added Javascript Source File named 2d_array-ds for HackerRank Challenge named 2D Array-DS

---
+++ Day_8/2d_array-ds.js
@@ -0,0 +1,12 @@
+function hourglassSum(arr) {
+
+    var hourSumArr = [];
+    var counter = 0;
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        for (let j = 1; j < arr[i].length - 1; j++) {
+            hourSumArr[counter++] = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
+        }
+    }
+    return Math.max(...hourSumArr);
+}
