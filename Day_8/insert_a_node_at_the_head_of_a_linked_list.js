12ec9c1604603d84485c00d63fa8ccde485ce262
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-28 21:53:17 +0530

    Added Javascript Source File named insert_a_node_at_the_head_of_a_linked_list.js for HackerRank Challenge named Insert a node at the head of a linked list

---
+++ Day_8/insert_a_node_at_the_head_of_a_linked_list.js
@@ -0,0 +1,9 @@
+function insertNodeAtHead(head, data) {
+    var node = new SinglyLinkedListNode(data);
+    node.next = null;
+    if (head == null) return node;
+    else {
+        node.next = head;
+        return node;
+    }
+}
