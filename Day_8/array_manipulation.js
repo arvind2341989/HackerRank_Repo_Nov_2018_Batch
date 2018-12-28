f862c37f5f32822399a39599c699c4e133c0007a
Rahul Kotwal <rkotwal8902@gmail.com>
2018-12-28 18:49:11 +0530

    Added Javascript Source File named array_manipulation.js for HackerRank Challenge named Array Manipulation

---
+++ Day_8/array_manipulation.js
@@ -0,0 +1,19 @@
+function arrayManipulation(n, queries) {
+    let arr = new Array(n).fill(0);
+    for(let i =0; i<queries.length; i++){
+        arr[queries[i][0]-1] += queries[i][2];
+        if (queries[i][1] < arr.length) {
+           arr[queries[i][1]] -= queries[i][2];
+        }
+    }
+    for (let j = 1; j < n; j++) {
+        arr[j] += arr[j-1];
+    }
+    let max=0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]>=max){
+            max = arr[i];
+        }
+    }
+    return max; 
+}
