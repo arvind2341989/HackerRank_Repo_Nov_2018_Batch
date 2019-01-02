67f9106bbb2d74c83b9340986db113a45556c837
Manisha Annamalaisamy <manisha665@gmail.com>
2019-01-02 11:19:13 +0530

    Added JavaScript Source File named Day_5/ashton_and_strings.js for HackerRank Challenge named Ashton and Strings.

---
+++ Day_5/ashton_and_strings.js
@@ -0,0 +1,20 @@
+function ashtonString(s, k) {
+    /*
+     * Write your code here.
+     */
+    const set = new Set();
+    var index = k;
+    for (let x = 0; x < s.length; x++) {
+        for (let y = 1; y <= s.length - x; y++) {
+            set.add(s.substr(x, y));
+        }
+    }
+
+    const arr = Array.from(set)
+
+    arr.sort();
+    console.log(arr);
+
+    return arr.join('')[index - 1];
+
+}
