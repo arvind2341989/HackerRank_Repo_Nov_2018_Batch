2039a3e264fbfdd09112444b6df693b988054dc1
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-03 21:57:30 +0530

    deleted all files

--- Day_3/sherlock_and_squares.js
+++
@@ -1,9 +0,0 @@
-function squares(a, b) {
-    var c = Math.sqrt(a);
-    var d = parseInt(Math.sqrt(a));
-    var sum = parseInt(Math.sqrt(b)) - parseInt(Math.sqrt(a));
-    if (c == d) {
-        sum = sum + 1;
-    }
-    return sum;
-}
