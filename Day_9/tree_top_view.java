23721b1f676d2e71a49838292de8a8c884b2c6d6
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-04 00:25:17 +0530

    Added Java Source File Named tree_top_view.java for HackerRank Challenge named Tree Top View.

---
+++ Day_9/tree_top_view.java
@@ -0,0 +1,31 @@
+	 static class QueueNode{
+        Node node;
+        int level;
+        public QueueNode(Node node, int level){
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+
+    public static void topView(Node root) {
+        Queue<QueueNode> queue = new LinkedList<QueueNode>();
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        queue.add(new QueueNode(root, 0));
+        while(!queue.isEmpty()){
+            QueueNode r = queue.poll();
+            if(!map.containsKey(r.level)){
+                map.put(r.level, r.node.data);
+            }
+            
+            if(r.node.left != null){
+                queue.add(new QueueNode(r.node.left, r.level - 1));
+            }
+            if(r.node.right != null){
+                queue.add(new QueueNode(r.node.right, r.level + 1));
+            }
+        }
+        for (Integer value : map.values()) {
+            System.out.print(value + " ");
+        }
+    }
