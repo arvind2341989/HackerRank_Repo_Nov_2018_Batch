af177e1422a1c3749577d5c40bfae1ee851f79c9
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-04 01:55:52 +0530

    Added JavaScript Source File Named jumping_on_the_clouds.js for HackerRank Challenge named Jumping On The Clouds.

---
+++ Day_3/jumping_on_the_clouds.js
@@ -0,0 +1,11 @@
+function jumpingOnClouds(c) {
+    var jumps = 0;
+    for (var i = 0; i < c.length-1; ){
+        if (c[i] != 1) {
+            jumps++;
+        }
+        i += (c[i + 2] == 0) ? 2 : 1;
+    }
+    return jumps;
+
+}
