ff0e597d3bd5b4f2bb29695e74a4597b25c26904
Kowshik Reddy Beeravolu <kowshikreddy137@gmail.com>
2019-01-03 20:56:16 +0530

    Added Java Source File Named find_merge_poins_of_two_lists.java for HackerRank Challenge named Find Merge Points Of Two lists.

---
+++ Day_8/find_merge_poins_of_two_lists.java
@@ -0,0 +1,22 @@
+   public static int dataposition = 0;
+    public static SinglyLinkedListNode start1;
+    public static SinglyLinkedListNode start2;
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode current1 = head1;
+        SinglyLinkedListNode current2 = head2;
+        while(current1 != current2){
+            if(current1.next == null){
+                current1 = head2;
+            }
+            else{
+                current1 = current1.next;
+            }
+            if(current2.next == null){
+                current2 = head1;
+            }
+            else{
+                current2 = current2.next;
+            }
+        }
+        return current1.data;
+    }
