c5a834e7377d0fadcdaac7b89bbd2c232aac6542
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-03 21:54:28 +0530

    deleted all files

--- Day_4/camel_case.js
+++
@@ -1,8 +0,0 @@
-function camelcase(s) {
-    var count = 0;
-    for (var i = 0; i < s.length; i++) {
-        if (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90)
-            count++;
-    }
-    return count + 1;
-}
