5598baaf212a6e1dc50982253395972bfcf4cddd
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-03 21:57:59 +0530

    deleted all files

--- Day_5/the_love_letter_mystery.js
+++
@@ -1,8 +0,0 @@
-function theLoveLetterMystery(s) {
-    var half = Math.floor(s.length / 2);
-    var diff = 0
-    for (let i = 0; i < half; i++) {
-        diff += Math.abs(s.charCodeAt(i) - s.charCodeAt(s.length - i - 1))
-    }
-    return diff;
-}
