da8f868cf4860699492724b7987cc4fad6148b9b
Ravali.Talluri <talluriravali@gmail.com>
2019-01-04 11:13:02 +0530

    Added Java script source file name day_10\poisonousplants.js  for HackerRank Challenge named Poisonous Plants.

---
+++ day_10/poisonousplants.js
@@ -0,0 +1,27 @@
+// Complete the poisonousPlants function below.
+function poisonousPlants(p) {
+    var maxDaysAlive = 0;
+    var stack = [];
+
+    for (var i = 0; i < p.length; i++) {
+        var daysAlive = 0;
+
+        while (stack.length > 0 && p[i] <= stack[stack.length - 1].plant)
+            daysAlive = Math.max(daysAlive, stack.pop().days);
+
+
+        if (stack.length === 0)
+            daysAlive = 0;
+
+        else
+            daysAlive += 1;
+
+        maxDaysAlive = Math.max(maxDaysAlive, daysAlive);
+
+        stack.push({
+            plant: p[i],
+            days: daysAlive
+        });
+    }
+    return maxDaysAlive;
+}
