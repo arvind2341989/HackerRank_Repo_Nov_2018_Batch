89b1e48fb18ffb94525593af479f3d2c75412c4f
MithraTalluri <tlns.mithra@gmail.com>
2019-01-04 15:36:59 +0530

    Added JavaScript Source File named pairs.js for HackerRank Challenge named Pairs

---
+++ Day_6/pairs.js
@@ -0,0 +1,14 @@
+function pairs(k, arr) {
+    arr.sort((a, b) => a - b);
+    var count = 0;
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = i; j < arr.length; j++) {
+            if (arr[j] - arr[i] > k) {
+                break;
+            } else if (arr[j] - arr[i] == k) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
