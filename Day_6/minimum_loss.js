2d4d1bc72c88c72d9b3573f21d348050687c65d4
MithraTalluri <tlns.mithra@gmail.com>
2019-01-04 15:36:40 +0530

    Added JavaScript Source File named minimum_loss.js for HackerRank Challenge named Minimum Loss

---
+++ Day_6/minimum_loss.js
@@ -0,0 +1,21 @@
+function minimumLoss(price) {
+    let yearsForPrices = [];
+
+    for (let i = 0; i < price.length; i++) {
+        yearsForPrices[price[i]] = i;
+    }
+
+    price.sort((a, b) => { return a - b });
+
+    let minPositionDifference = Number.MAX_VALUE;
+
+    for (let i = 0; i < price.length - 1; i++) {
+        if (yearsForPrices[price[i + 1]] < yearsForPrices[price[i]]) {
+            let diff = price[i + 1] - price[i];
+            if (diff > 0) {
+                minPositionDifference = Math.min(minPositionDifference, diff);
+            }
+        }
+    }
+    return minPositionDifference;
+}
