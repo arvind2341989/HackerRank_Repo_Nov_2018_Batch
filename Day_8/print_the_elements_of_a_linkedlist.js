eb399383889ee4e20b795a7e4669d2cd0155a93c
MithraTalluri <tlns.mithra@gmail.com>
2019-01-04 15:59:36 +0530

    Added JavaScript Source File named print_the_elements_of_a_linkedlist.js for HackerRank Challenge named Print The Elements Of A Linked List

---
+++ Day_8/print_the_elements_of_a_linkedlist.js
@@ -0,0 +1,8 @@
+function printLinkedList(head) {
+    let next = head.next;
+    console.log(head.data);
+    while (next != null) {
+        console.log(next.data);
+        next = next.next;
+    }
+}
