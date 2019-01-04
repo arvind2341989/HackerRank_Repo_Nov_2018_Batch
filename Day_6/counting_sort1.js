f557319475232fbcae129c63d44e0ebc0eaca14c
MithraTalluri <tlns.mithra@gmail.com>
2019-01-04 14:07:37 +0530

    Added JavaScript Source File named counting_sort1.js for HackerRank Challenge named Counting Sort 1

---
+++ Day_6/counting_sort1.js
@@ -0,0 +1,10 @@
+// Complete the countingSort1 function below.
+function countingSort(arr) {
+
+    let counts = Array(100).fill(0);
+
+    arr.forEach(item => { ++counts[item]; });
+
+    return counts;
+
+}
