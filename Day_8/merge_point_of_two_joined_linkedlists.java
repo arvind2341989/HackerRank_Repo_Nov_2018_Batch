3abc08515efd9515a99f3b4dd24fd29bbe773929
MithraTalluri <tlns.mithra@gmail.com>
2019-01-04 15:58:24 +0530

    Added Java Source File named merge_point_of_two_joined_linkedlists.java for HackerRank Challenge named Merge Point Of Two Joined Linked Lists

---
+++ Day_8/merge_point_of_two_joined_linkedlists.java
@@ -0,0 +1,33 @@
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    public static int dataposition = 0;
+    public static SinglyLinkedListNode start1;
+    public static SinglyLinkedListNode start2;
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode current1 = head1;
+        SinglyLinkedListNode current2 = head2;
+        while(current1 != current2){
+            if(current1.next == null){
+                current1 = head2;
+            }
+            else{
+                current1 = current1.next;
+            }
+            if(current2.next == null){
+                current2 = head1;
+            }
+            else{
+                current2 = current2.next;
+            }
+        }
+        return current1.data;
+    }
