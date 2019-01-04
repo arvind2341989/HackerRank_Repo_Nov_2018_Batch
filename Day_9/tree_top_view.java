1b8d1fdabcc620ac089577c42f7171a3c68358b2
MithraTalluri <tlns.mithra@gmail.com>
2019-01-04 17:59:21 +0530

    Added Java Source File named tree_top_view.java for HackerRank Challenge named Tree: Top View

---
+++ Day_9/tree_top_view.java
@@ -0,0 +1,55 @@
+	/* 
+    
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+
+    //new class to store level of each node along with the node 
+	 static class QueueNode{
+        Node node;
+        int level;
+        public QueueNode(Node node, int level){
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+
+    public static void topView(Node root) {
+        //took a queue - similar to BFS approach
+        Queue<QueueNode> queue = new LinkedList<QueueNode>();
+        
+        //why TreeMap? Because I want the values to be sorted from leftmost to rightmost nodes
+        //Eg: TreeMap<first, second>
+        //first - stores the level of the node
+        //second - stores the node.data
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        
+        //start (since root, level = 0)
+        queue.add(new QueueNode(root, 0));
+        
+        while(!queue.isEmpty()){
+            //remove element from queue
+            QueueNode r = queue.poll();
+            //if the level of this node has never been reached before -> visible in top view
+            if(!map.containsKey(r.level)){
+                map.put(r.level, r.node.data);
+            }
+            
+            //add node's descendants
+            //all left child node levels = node.level - 1
+            //all right child node levels = node.level + 1
+            if(r.node.left != null){
+                queue.add(new QueueNode(r.node.left, r.level - 1));
+            }
+            if(r.node.right != null){
+                queue.add(new QueueNode(r.node.right, r.level + 1));
+            }
+        }
+        //since already sorted (cuz TreeMap), print from left to right
+        for (Integer value : map.values()) {
+            System.out.print(value + " ");
+        }
+    }
