087836753bbe24b4943387979810b0019c0a6cc3
Ravali.Talluri <talluriravali@gmail.com>
2019-01-08 12:25:21 +0530

    Added Java script source file name day_9\array_pairs.js  for HackerRank Challenge named Array Pairs.

---
+++ day_9/array_pairs.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(arr) {
+    var count = 0, pairs = [];
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            var max = Math.max(...arr.slice(i, j + 1));
+            if (arr[i] * arr[j] <= max) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = solve(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
