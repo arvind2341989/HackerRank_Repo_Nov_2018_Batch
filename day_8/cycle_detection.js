497957e8d1af402ed42182958e7f57126b43665b
Ravali.Talluri <talluriravali@gmail.com>
2019-01-08 12:31:37 +0530

    Added Java script source file name day_8\cycle_detection.js  for HackerRank Challenge named Cycle Detection.

---
+++ day_8/cycle_detection.js
@@ -0,0 +1,16 @@
+static boolean hasCycle(SinglyLinkedListNode head) {
+SinglyLinkedListNode temp = head;
+boolean flag = false;
+while(temp.next != null)
+{
+    if(temp.data== -1){
+        flag = true;
+        break;
+    }
+    else{
+        temp.data= -1;
+    }
+    temp = temp.next;
+}
+return flag;
+    }
