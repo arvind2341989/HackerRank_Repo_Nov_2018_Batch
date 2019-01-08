bb78ef0b9b40d42de46960a90d8865a34cdf0dcc
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-08 13:24:27 +0530

    Added JavaScript Source File Named ashton_and_string.js for HackerRank Challenge named Ashton And String.

---
+++ Day_5/ashton_and_string.js
@@ -0,0 +1,17 @@
+function ashtonString(s, k) {
+
+    const set = new Set();
+    var index = k;
+    for (let x = 0; x < s.length; x++) {
+        for (let y = 1; y <= s.length - x; y++) {
+            set.add(s.substr(x, y));
+        }
+    }
+
+    const arr = Array.from(set)
+
+    arr.sort();
+    console.log(arr);
+
+    return arr.join('')[index - 1];
+}
