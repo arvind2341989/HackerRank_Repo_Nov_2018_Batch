4dff1851e2a8261c70c2a8a9b88b02ded81e27ee
Kowshik Reddy Beeravolu <kowshikreddy137@gmail.com>
2019-01-16 16:20:33 +0530

    Added Java Source File Named Java_Date_and_Time.java for HackerRank Challenge named Java Date and Time.

---
+++ Day_20/Java_Date_and_Time.java
@@ -0,0 +1,22 @@
+class Java_Date_and_Time {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+    
+       Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+
+        return (new DateFormatSymbols().getWeekdays()[calendar.get(Calendar.DAY_OF_WEEK)].toUpperCase());
+    
+    }
+
+}
