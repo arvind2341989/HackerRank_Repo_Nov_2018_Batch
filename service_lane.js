0c768883fec9d6fd27ed5c9712bdc3807dbf5d1e
Sainath Rao Mukhirala <saimukhirala@gmail.com>
2019-01-16 10:34:08 +0530

    Added Java Source File named ifelse.java for HackerRank Challenge named if else.

--- service_lane.js
+++
@@ -1,9 +0,0 @@
-function findMin(lanes, i, j) {
-    var min = 10000000;
-    for (var k = i; k <= j; k++) {
-        if (lanes[k] < min) {
-            min = lanes[k];
-        }
-    }
-    return min;
-}
