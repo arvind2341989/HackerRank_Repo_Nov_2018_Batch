1ac721e32373733cbe6236bbbe4aec28d15c3c77
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-16 15:36:06 +0530

    Added Java Source File Named java_strings_introduction.java for HackerRank Challenge named Java Strings Introduction.

---
+++ Day_20/java_strings_introduction.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        System.out.println(A.length() + B.length());
+
+
+        System.out.println((0 >= (A.compareTo(B))) ? "No" : "Yes");
+      
+      
+        System.out.println((Character.toUpperCase(A.charAt(0)) + A.substring(1))+" "+Character.toUpperCase(B.charAt(0)) + B.substring(1));
+
+    }
+}
+
+
