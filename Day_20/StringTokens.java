2e36df28941c90244f591f2fafcfa7dd7a44b415
Sainath Rao Mukhirala <saimukhirala@gmail.com>
2019-01-16 15:30:34 +0530

    Added Java Source File named StringTokens.java for HackerRank Challenge named String tokens.

---
+++ Day_20/StringTokens.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class StringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+            StringTokenizer st=new StringTokenizer(s," !,?._'@");
+            System.out.println(st.countTokens());
+            while(st.hasMoreTokens()){
+                System.out.println(st.nextToken());
+        scan.close();
+    }
+}
+}
+
