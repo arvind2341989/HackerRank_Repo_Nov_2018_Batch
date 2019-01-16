c0f66504d807bf50673a7fc4972c34e4b9800f48
indraneel rachakonda <indhraneelrachakonda@gmail.com>
2019-01-16 15:27:44 +0530

    Added Java Source File Named Java_end_of_file.java for HackerRank Challenge named Java End Of File.

---
+++ Day_20/Java_end_of_file.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaEndOfFile{
+    private static final Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int i = 1;
+        while(scan.hasNext()){
+            String line = scan.nextLine();
+            System.out.println(i + " " + line);
+            i++;
+        }
+    }
+}
