2d87e04cb3fd4e594b5e93e9a24a7550b49b1b98
Kowshik Reddy Beeravolu <kowshikreddy137@gmail.com>
2019-01-16 16:23:15 +0530

    Added Java Source File Named Java_End-of-file.java for HackerRank Challenge named Java End-of-file.

---
+++ Day_20/Java_End-of-file.java
@@ -0,0 +1,18 @@
+public class Java_End-of-file {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner in = new Scanner(System.in);
+
+        String line, text = "";
+        int i = 1;
+
+        while( in.hasNextLine() != false){
+            line = in.nextLine();
+            text = text + Integer.toString(i) + " " + line +"\n";
+            i++;
+        }
+        System.out.println(text);
+    }
+}
