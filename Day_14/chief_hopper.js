254fc4885c028aa684186187c8b929d1a9501490
Rahul Kotwal <rkotwal8902@gmail.com>
2019-01-16 18:48:57 +0530

    Added Javascript Source File named chief_hopper.js for HackerRank Challenge named Chief Hopper

---
+++ Day_14/chief_hopper.js
@@ -0,0 +1,15 @@
+// Complete the chiefHopper function below.
+function chiefHopper(arr) {
+    var startEnergy = 0;
+    var done = false;
+    while (!done) {
+        let energy = startEnergy;
+        for (var i = 0; i < arr.length && energy >= 0; i++){
+            if (arr[i] > energy) energy -= arr[i] - energy;
+            else if (arr[i] < energy) energy += energy - arr[i];
+        }
+        if (i == arr.length && energy >= 0) done = true;
+        else startEnergy++;
+    }
+    return startEnergy;
+}
