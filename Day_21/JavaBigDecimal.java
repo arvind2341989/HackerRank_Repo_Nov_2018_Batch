2e87bcd945a8aadda2274821254bc7a84d8b0068
Rahul Kotwal <rkotwal8902@gmail.com>
2019-01-17 16:56:03 +0530

    Added Java Source File named JavaBigDecimal.java for HackerRank Challenge named Java Big Decimal

---
+++ Day_21/JavaBigDecimal.java
@@ -0,0 +1,37 @@
+import java.math.BigDecimal;
+import java.util.*;
+class JavaBigDecimal{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        for(int i = 0;i < n; i++){
+            BigDecimal max = new BigDecimal(s[i]);
+            int index = i;
+
+            for(int j = i+1; j < n; j++){
+                BigDecimal tempMax = new BigDecimal(s[j]);
+                if(tempMax.compareTo(max)==1){
+                    max = tempMax;
+                    index = j;
+                }
+            }
+            String temp = s[i];
+            s[i] = s[index];
+            s[index] = temp;
+        }
+        
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
