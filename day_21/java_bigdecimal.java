7201402bf715019c17cf898d7f1bbe79810f7c1a
Ravali.Talluri <talluriravali@gmail.com>
2019-01-17 18:44:41 +0530

    Added Java script source file name day_21\java_bigdecimal.java  for HackerRank Challenge named Java BigDecimal.

---
+++ day_21/java_bigdecimal.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+Arrays.sort(s, 0, n, (String s1, String s2) -> {
+            BigDecimal n1 = new BigDecimal(s1);
+            BigDecimal n2 = new BigDecimal(s2);
+            return n2.compareTo(n1);
+        });
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
