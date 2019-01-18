973fb9ef4516fd7c31ed7c651007d621059d6f10
Rahul Kotwal <rkotwal8902@gmail.com>
2019-01-18 17:46:18 +0530

    Added Javascript Source File named stock_maximize.js for HackerRank Challenge named Stock Maximize

---
+++ Day_16/stock_maximize.js
@@ -0,0 +1,11 @@
+// Complete the stockmax function below.
+function stockmax(prices) {
+    let max = 0, profit = 0;
+    for (let i = prices.length-1; i > -1; i--){
+        if (prices[i] > max) {
+            max = prices[i];
+        }
+        profit += max - prices[i];
+    }
+    return profit;
+}
