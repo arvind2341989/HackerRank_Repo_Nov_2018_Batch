b89c2b44dd5210332c9f78bb47adb9ea7ad6394e
Rahul Kotwal <rkotwal8902@gmail.com>
2019-01-18 10:13:25 +0530

    Added Javascript Source File named abbreviation.js for HackerRank Challenge named Abbreviation

---
+++ Day_16/abbreviation.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the abbreviation function below.
+function abbreviation(a, b) {
+    let mem = Array(a.length + 1).fill(0).map(_ => Array(b.length + 1).fill(false));
+    mem[a.length][b.length] = true;
+    let i = a.length - 1;
+    while (i > -1 && a[i] === a[i].toLowerCase()) {
+        mem[i--][b.length] = true;
+    }
+    while (i > -1) {
+        mem[i--][b.length] = false;
+    }
+    let j = b.length - 1;
+    while (j > -1 && b[j] === a[j].toLowerCase()) {
+        mem[a.length][j--] = true;
+    }
+    while (j > -1) {
+        mem[a.length][j--] = false;
+    }
+    for (let i = a.length - 1; i > -1; i--) {
+        for (let j = b.length - 1; j > -1; j--) {
+            if (a[i].toLowerCase() === b[j].toLowerCase()) {
+                mem[i][j] = mem[i][j] || mem[i+1][j+1];
+            }
+            if (a[i] === a[i].toLowerCase()) {
+                mem[i][j] = mem[i][j] || mem[i + 1][j];
+            }
+            if (b[j] === b[j].toLowerCase()) {
+                mem[i][j] = mem[i][j] || mem[i][j + 1];
+            }
+        }
+    }
+    console.log(mem);
+    return mem[0][0] ? 'YES' : 'NO';
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const a = readLine();
+
+        const b = readLine();
+
+        let result = abbreviation(a, b);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
