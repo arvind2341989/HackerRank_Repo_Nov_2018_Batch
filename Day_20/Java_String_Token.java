53ca57499463e86d745a045caa6c3d9c97ed2c67
Aparajith Ragunathan <aparajith.r@tadigital.com>
2019-01-18 09:53:55 +0530

    Added Java Source File Named Java_String_Token.java for HackerRank Challenge named Java String Token.

---
+++ Day_20/Java_String_Token.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+public class Solution {
+
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine().trim();
+        scanner.close();
+
+        if (string.isEmpty())
+            System.out.println("0");
+
+        else {
+            String[] array = string.split("[ !,?\\.\\_'@]+");
+
+            System.out.println(array.length);
+
+            for (int i = 0; i < array.length && array[i] != null; ++i)
+                System.out.println(array[i]);
+        }
+    }
+
+}
+
