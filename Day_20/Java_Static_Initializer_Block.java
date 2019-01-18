841c11ea83033bbe06c178a85a974b3652a18682
Aparajith Ragunathan <aparajith.r@tadigital.com>
2019-01-18 09:51:54 +0530

    Added Java Source File Named Java_Static_Initializer_Block.java for HackerRank Challenge named Java Static Initializer Block.

---
+++ Day_20/Java_Static_Initializer_Block.java
@@ -0,0 +1,17 @@
+
+public static int B;
+
+public static int H;
+
+public static boolean flag;
+static{
+    Scanner sc=new Scanner(System.in);
+    B=sc.nextInt();
+    H=sc.nextInt();
+    if(B<=0||H<=0){
+        flag=false;
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }else{
+        flag=true;
+    }
+}
