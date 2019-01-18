2e9c65509fcb11d16468211eb2538a403f05171c
Rahul Kotwal <rkotwal8902@gmail.com>
2019-01-18 10:18:31 +0530

    Added Javascript Source File named sam_and_substrings.js for HackerRank Challenge named Sam and Substrings

---
+++ Day_16/sam_and_substrings.js
@@ -0,0 +1,12 @@
+// Complete the substrings function below.
+function substrings(n) {
+    let s = n.split('');
+    let res = 0;
+    let f = 1;
+    let l = s.length;
+    for (let i = l - 1; i >= 0; i--) {
+        res = (res + (s[i] - '0') * f * (i + 1)) % 1000000007;
+        f = (f * 10 + 1) % 1000000007;
+    }
+    return res;
+}
