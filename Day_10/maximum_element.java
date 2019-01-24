6093ded54b883a61f082dcacb88bc2d9b67677fc
Manisha Annamalaisamy <manisha665@gmail.com>
2019-01-24 14:39:31 +0530

    Added JavaScript Source File named maximum_element.java for HackerRank Challenge named Maximum Element.

---
+++ Day_10/maximum_element.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+   
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         static class StackNode{
+        int val;
+        StackNode(int val){
+            this.val = val;
+        }
+    }
