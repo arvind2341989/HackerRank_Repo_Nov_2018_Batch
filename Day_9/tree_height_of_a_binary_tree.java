c105f26ab2618ad04e4c1f255d334f35df4c6cc6
Manisha Annamalaisamy <manisha665@gmail.com>
2019-01-24 12:04:45 +0530

    Added Java Source File named tree_height_of_a_binary_tree.java for HackerRank Challenge named Tree height of a binary tree.

---
+++ Day_9/tree_height_of_a_binary_tree.java
@@ -0,0 +1,11 @@
+public static int height(Node root) {
+      	// Write your code here.
+               if (root== null) {
+            return -1; 
+          }
+        else {
+         
+                return Math.max(height(root.left),height(root.right))+1;
+            
+        }
+    }
