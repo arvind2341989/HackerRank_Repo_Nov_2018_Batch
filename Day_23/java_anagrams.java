4fcf7bb5c6cc28b9ca8e0a1f0fb7991f3b71c690
Pavan Kumar Reddy Chinta <c.pavankumarreddy@yahoo.com>
2019-02-07 12:49:05 +0530

    Added JavaScript Source File named java_anagrams.java for HackerRank Challenge named java anagrams.

---
+++ Day_23/java_anagrams.java
@@ -0,0 +1,24 @@
+static boolean isAnagram(String a, String b) {
+        if(a.length() != b.length()){
+            return false;
+        } else if (a.toUpperCase().equals(b.toUpperCase())) {
+            return true;
+        } else {
+            int alpha[] = new int[26];
+            for (int i = 0; i < a.length(); i++) {
+                alpha[a.toUpperCase().charAt(i) - 65]++;
+                alpha[b.toUpperCase().charAt(i) - 65]--;
+            }
+            int res = 0;
+            for (int i = 0; i < 26; i++) {
+                if (alpha[i] != 0) {
+                    res++;
+                } 
+            }             
+            if (res == 0) {
+                return true;
+            } else {
+                return false;
+            }       
+        }
+    }
