8ab5b8bcf5fcbd38a9963ffc1fec2f590b526816
Pavan Kumar Reddy Chinta <c.pavankumarreddy@yahoo.com>
2019-02-07 12:28:40 +0530

    Added JavaScript Source File named reverse_shuffle.js for HackerRank Challenge named reverse shuffle.

---
+++ Day_15/reverse_shuffle.js
@@ -0,0 +1,30 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// Complete the reverseShuffleMerge function below.
+function reverseShuffleMerge(s) {
+    var { dict, skipDict } = buildDictionary(s);
+    var charList = [];
+    s.split('').reverse().forEach((c) => {
+        if (charList.length === 0) {
+            charList.push(c);
+            dict[c]--;
+        } else {
+            if (dict[c] == 0) {
+                skipDict[c]--;
+            } else {
+                while (charList.length > 0) {
+                    let last = charList[charList.length - 1];
+                    if (c < last && skipDict[last] > 0) {
+                        skipDict[last]--;
+                        dict[last]++;
+                        charList.length--;
+                    } else {
+                        break;
+                    }
+                }
+                charList.push(c);
+                dict[c]--;
+            }
+        }
+    });
+    return charList.join('');
+}
