2782da08e9822a312125751558442ada73505ef7
nithilareddy <Nithilareddy.g@tadigital.com>
2019-02-07 13:16:16 +0530

    Added JavaScript File called java_string_comparison.java for the HackerRank Challenge called Java String Comparison

---
+++ Day_22/java_string_comparison.java
@@ -0,0 +1,15 @@
+public static String getSmallestAndLargest(String s, int k) {
+        
+       String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);    
+        for (int i = 0; i <= s.length() - k; i++) {
+            String subString= s.substring(i, i + k) ;
+            if(subString.compareTo(smallest) < 0) {
+                smallest=subString;
+            }
+            else if (subString.compareTo(largest) >= 0) {
+                largest = subString;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
